b233639d2d48d58d59cd4d26e34286af611c4396
Dev <dev@Devs-MacBook-Pro.local>
2022-09-15 12:53:30 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="burgerContent">
+      Hi
+    </div>
+  );
+}
+
+export default App;
